41dccfcfd1bb20add2b0b207c5d621a1d73fbc2c
Daiki Ihara <sasurau4@gmail.com>
2019-06-13 12:05:53 +0900

    add README

---
+++ README.md
@@ -0,0 +1,13 @@
+## sample-rnweb
+
+Sample repository of react-native-web.
+
+```
+yarn install
+yarn start
+```
+
+Access to `http://localhost:8080/` from browser.
+
+Welcome to React Native Web's world.
+
--- index.jsx
+++ index.jsx
@@ -32,14 +32,6 @@ function Hoge() {
         <Path d="M 40 60 A 10 10 0 0 0 60 60" stroke="black" />
       </Svg>
       */}
-      <Text>With style</Text>
-      <Svg height="100" width="100" style={styles.svg}>
-        <Rect x="0" y="0" width="100" height="100" fill="black" />
-        <Circle cx="50" cy="50" r="30" fill="yellow" />
-        <Circle cx="40" cy="40" r="4" fill="black" />
-        <Circle cx="60" cy="40" r="4" fill="black" />
-        <Path d="M 40 60 A 10 10 0 0 0 60 60" stroke="black" />
-      </Svg>
     </View>
   );
 }
